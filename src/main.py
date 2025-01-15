49cf0b33542bec21c302743fba882ce13843b055
ht-yarll <h.scott.xp@gmail.com>
2025-01-15 13:49:45 -0300

    :recycle: refactor: review code

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 import pathlib
 from api.gupy import gupy_fetch_data
+
 from modules.GBigquery import GBigQuery, get_bqclient
 from modules.GCStorage import GCStorage, get_gclient
 from modules.DataProcessor import DataProcessor
+
 from google.cloud import bigquery
 
 working_dir = pathlib.Path.cwd()
@@ -31,79 +33,6 @@ try:
 except Exception as e:
     print(e)
 
-#Download from CloudStorage
-gcs_demo_blobs = gcs.list_blobs(bucket_gcs)
-downloads_folder = working_dir.joinpath('data/downloads')
-
-for blob in gcs_demo_blobs:
-    path_download = downloads_folder.joinpath(blob.name)
-    if not path_download.parent.exists():
-        path_download.parent.mkdir(parents=True)
-
-    try:    
-        blob.download_to_filename(str(path_download))
-        print(f'Download of {blob.name} was done!')
-        blob.delete()
-        print(f'Disposing of {blob.name}')
-    except Exception as e:
-        print(e)
-
-#Sending to BigQuery
+#Streaming to BigQuery
 bq_client = get_bqclient()
-bqc = GBigQuery(bq_client)
-project_id = 'blackstone-446301'
-dataset_id = 'user_data'
-table_name = 'gupy_data'
-destination_table = f'{project_id}.{dataset_id}.{table_name}'
-
-for file in downloads_folder.iterdir():
-    try:
-        bqc.up_to_bigquery(file, destination_table=destination_table)
-        print(f'Upload of {file.name} was done!')
-    except Exception as e:
-        print(f'Error in upload:{e}')
-
-#Querying BigQuery
-excluded_columns = [
-    'description', 'careerpagelogo', 'city', 'joburl', 'badges', 'careerpageurl'
-    ]
-table_id = 'blackstone-446301.user_data.gupy_data'
-table = bq_client.get_table(table_id)
-selected_columns = [
-    field.name for field in table.schema 
-    if field.name not in excluded_columns
-    ]
-
-query_job_select = f"""
-SELECT {', '.join(selected_columns)}
-FROM   `blackstone-446301.user_data.gupy_data`
-WHERE country = ('Brasil')
-"""
-update_query = f"""UPDATE `blackstone-446301.user_data.gupy_data_to_analyze`
-SET type = CASE
-    WHEN type LIKE '%vacancy_type_effective%' THEN 'efetivo'
-    WHEN type LIKE '%vacancy_type_internship%' THEN 'estagio'
-    WHEN type LIKE '%vacancy_legal_entity%' THEN 'pj'
-    WHEN type LIKE '%vacancy_type_apprentice%' THEN 'aprendiz'
-    WHEN type LIKE '%vacancy_type_lecturer%' THEN 'professor'
-END
-WHERE country = ('Brasil')
-"""
-new_table_name = 'gupy_data_to_analyze'
-
-if not new_table_name in bqc.list_tables(dataset_id):
-    print(f'Creating table {new_table_name} on {dataset_id}...')
-    bqc.query(
-        query_job_select, 
-        destination_table=f'{project_id}.{dataset_id}.{new_table_name}',
-        write_disposition = 'WRITE_TRUNCATE'
-            ) 
-else:
-    print(f'Table {new_table_name} already exists on {dataset_id}')
-    bqc.query(
-        query_job_select, 
-        destination_table = f'{project_id}.{dataset_id}.{new_table_name}',
-        write_disposition = 'WRITE_TRUNCATE'
-        )
-bqc.query(update_query)   
-print('Process finished')
+gbq = GBigQuery(bq_client)
